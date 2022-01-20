59efde182e7adc11ded33156b49645112dfa7faf
LoReeS23 <mikolajszulc1@gmail.com>
2022-01-20 13:50:55 +0100

    views

--- characters/views.py
+++ characters/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
 from django.views import View
+from models import *
 
 
 class ShowMain(View):
-    pass
+    def get(self, request):
+        return render(request, 'main.html', {'current_pos': CURRENT_POSITION})
+
+    def post(self, request):
+        pass
