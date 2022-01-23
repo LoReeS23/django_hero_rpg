82bc7e2fb1e35e9b19be14fe392834eeb14a2a07
LoReeS23 <mikolajszulc1@gmail.com>
2022-01-23 20:24:13 +0100

    Update urls.py

--- django_hero_rpg/urls.py
+++ django_hero_rpg/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from characters.views import *
+from django.urls import path, include
+
+import user
+from game.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', ShowMain.as_view()),
+    path('user/', include('user.urls'))
 ]
