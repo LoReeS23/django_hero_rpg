06a8f450f1d764abf1ba64aca17905784e05eec8
LoReeS23 <mikolajszulc1@gmail.com>
2022-01-23 19:36:12 +0100

    ShowUserPage new view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class ShowUserPage(View):
+    def get(self, request):
+        return render(request, "user_page.html")
+
+    def post(self, request):
+        pass
