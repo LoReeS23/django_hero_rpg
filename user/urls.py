2144bd272d117c5480460cdee4386b9139ac0e9b
LoReeS23 <mikolajszulc1@gmail.com>
2022-01-23 20:24:29 +0100

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from user.views import *
 
 urlpatterns = [
-    path('', ShowUserPage.as_view())
+    path('', ShowUserPage.as_view()),
+    path('create/', CreateUser.as_view())
 ]
