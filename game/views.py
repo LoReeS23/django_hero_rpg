46fac6c82c887ae4b7cc7eb2c1736e4c31c0ca9d
LoReeS23 <mikolajszulc1@gmail.com>
2022-01-23 19:17:50 +0100

    new app game made

---
+++ game/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import View
+from characters.models import *
+
+
+class ShowMain(View):
+    def get(self, request):
+        pos_x = Hero.objects.all()
+        pos_y = Hero.objects.all()
+        return render(request, 'main.html', {'pos_x': pos_x,
+                                             'pos_y': pos_y})
+
+    def post(self, request):
+        if "find_monsters" in request.POST:
+            pass
+        if "go_to_city" in request.POST:
+            pass
