d15243165e79e197e8e9bb0af9f23cd3cac76b6a
LoReeS23 <mikolajszulc1@gmail.com>
2022-01-23 20:24:28 +0100

    Update models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from user.forms import SEX_CHOICE
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser):
+    nickname = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    re_password = models.CharField(max_length=50)
+    sex = models.CharField(max_length=20, choices=SEX_CHOICE)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
